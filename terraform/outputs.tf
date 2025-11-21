cddef3de2d21c21c5581551162911c2441e04ab6
Uro <uro@Uros-MacBook-Pro.local>
2025-11-21 10:54:22 -0600

    ci: prefer terraform output for backup bucket; add output app_backup_bucket_name

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -14,3 +14,8 @@ output "kubeconfig" {
   value = module.eks.kubeconfig
   sensitive = true
 }
+
+output "app_backup_bucket_name" {
+  description = "S3 bucket name used for app backups"
+  value       = aws_s3_bucket.app_backup.bucket
+}
