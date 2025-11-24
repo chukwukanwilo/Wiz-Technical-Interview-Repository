298e231a779330e115ea1936bba4eca0df695f88
Chucks Nwilo <chukwukanwilo@gmail.com>
2025-11-24 16:49:32 +0000

    Fix MongoDB VM: add public IP for package downloads

--- terraform/s3_ec2_iam.tf
+++ terraform/s3_ec2_iam.tf
@@ -142,9 +142,10 @@ resource "aws_security_group" "mongo_sg" {
 
 # EC2 instance for Mongo
 resource "aws_instance" "mongo_vm" {
-  ami                    = data.aws_ami.amazon_linux_2.id
-  instance_type          = "t3.micro"
-  subnet_id              = module.vpc.public_subnets[0]
+  ami                         = data.aws_ami.amazon_linux_2.id
+  instance_type               = "t3.micro"
+  subnet_id                   = module.vpc.public_subnets[0]
+  associate_public_ip_address = true
   # key_name removed - SSH port is open but no keypair (create manually if needed)
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
   vpc_security_group_ids = [aws_security_group.mongo_sg.id]
