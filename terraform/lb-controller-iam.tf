d1497dbc6e9d5153e6181fc6fd3e122cc3ad7452
Chucks Nwilo <chukwukanwilo@gmail.com>
2025-11-24 17:46:28 +0000

    Add DescribeListenerAttributes permission to Load Balancer Controller

--- terraform/lb-controller-iam.tf
+++ terraform/lb-controller-iam.tf
@@ -63,6 +63,7 @@ resource "aws_iam_policy" "aws_load_balancer_controller" {
           "elasticloadbalancing:DescribeLoadBalancers",
           "elasticloadbalancing:DescribeLoadBalancerAttributes",
           "elasticloadbalancing:DescribeListeners",
+          "elasticloadbalancing:DescribeListenerAttributes",
           "elasticloadbalancing:DescribeListenerCertificates",
           "elasticloadbalancing:DescribeSSLPolicies",
           "elasticloadbalancing:DescribeRules",
